873b73265ae2fc90f9032b35a9935e1035e0c2d4
LocNguyen <LocNguyen@SG-DELL-18>
2023-06-17 20:28:47 -0400

    fix UI for welcome screen

--- components/shared.tsx
+++ components/shared.tsx
@@ -1,12 +1,7 @@
-import { Dimensions } from "react-native";
-import styled from "styled-components/native";
-import { colors } from "./colors";
+import { Dimensions, View } from "react-native";
+import { styled } from "nativewind";
 
-export const Container = styled.View`
-  flex: 1;
-  align-items: center;
-  background-color: ${colors.white};
-`;
+export const Container = styled(View, "flex-1 items-center bg-white");
 
 export const ScreenWidth = Dimensions.get("screen").width;
 export const ScreenHeight = Dimensions.get("screen").height;
--- screens/Welcome.tsx
+++ screens/Welcome.tsx
@@ -1,38 +1,20 @@
 import React, { FunctionComponent } from "react";
 import { StatusBar } from "expo-status-bar";
-import styled from "styled-components/native";
+import { styled } from "nativewind";
 
 import { Container } from "../components/shared";
-import { colors } from "../components/colors";
 import BigText from "../components/Texts/BigText"; // curly braces for named export
 import SmallText from "../components/Texts/SmallText"; // no curly braces only for default export
 import RegularButton from "../components/Buttons/RegularButton";
 
-const WelcomeContainer = styled(Container)`
-  background-color: ${colors.primary};
-  justify-content: space-between;
-  width: 100%;
-  height: 100%;
-`;
+const WelcomeContainer = styled(
+  Container,
+  "bg-orange-200 justify-between w-full h-full"
+);
 
-const TopSection = styled.View`
-  width: 100%;
-  flex: 1;
-  max-height: 55%;
-`;
-
-const BottomSection = styled.View`
-  width: 100%;
-  padding: 25px;
-  flex: 1;
-  justify-content: flex-end;
-`;
-
-const TopImage = styled.Image`
-  width: 100%;
-  height: 100%;
-  resize-mode: stretch;
-`;
+const TopSection = styled(View, "w-full flex-1 max-h-full");
+const BottomSection = styled(View, "w-full p-7 flex-1 justify-end");
+const TopImage = styled(Image, "object-fill w-full h-full");
 
 //image
 import background from "./../assets/background/background_welcome.jpg";
@@ -40,6 +22,7 @@ import background from "./../assets/background/background_welcome.jpg";
 // types
 import { AppStackParamList } from "../navigators/AppStack";
 import { StackScreenProps } from "@react-navigation/stack";
+import { View, Image } from "react-native";
 type Props = StackScreenProps<AppStackParamList, "Welcome">;
 
 const Welcome: FunctionComponent<Props> = ({ navigation }) => {
